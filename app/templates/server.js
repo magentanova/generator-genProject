273b190cf076550b94ff00441168f9f5c53bf71f
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-23 14:57:46 -0600

    fixed spelling errors in server.js

--- app/templates/server.js
+++ app/templates/server.js
@@ -1,23 +1,23 @@
-var http = require('http') 
-var fs = require('fs') 
+var http = require('http')
+var fs = require('fs')
 var port = 3000
 
-var requestHandler = function(request, response) {  
+var requestHandler = function(request, response) {
 	var filename = request.url === '/' ? 'index.html' : process.cwd() + request.url
 	console.log('sending ' + filename)
 	var strm = fs.readFile(filename, (err,contents) => {
 		if (err) {
-			response.statusMessage = "what?! poor you we didn't found yoru file!"
+			response.statusMessage = "what?! poor you we didn't find your file!"
 			response.statusCode = 404
 			response.end()
 		}
-		else response.end(contents) 
+		else response.end(contents)
 	})
 }
 
 var server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
